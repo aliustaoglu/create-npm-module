a4e1adee4f244356da6f2d7ecbd1e9e0dddfa67e
Cuneyt Aliustaoglu <cuneyt.aliustaoglu@accordo.com>
2017-11-13 12:19:16 +1300

    Fixed the line break issues with MacOS

--- bin/create-npm-module.js
+++ bin/create-npm-module.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
+
 var myLibrary = require('../lib/index.js');
 var args = process.argv.splice(process.execArgv.length + 2);
-if (args.length !== 1){
-  console.log("Wrong arguments. Please enter package name");
+if (args.length !== 1) {
+  console.log('Wrong arguments. Please enter package name');
   return -1;
 }
 
